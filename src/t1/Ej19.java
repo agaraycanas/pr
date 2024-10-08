49ddfb4309409749ba6e946489ec31477055ef04
HARRY <HARRY@potter>
2024-10-08 10:13:33 +0200

    Ej1-21

---
+++ src/t1/Ej19.java
@@ -0,0 +1,21 @@
+package t1;
+
+import java.util.Scanner;
+
+public class Ej19 {
+	
+	public static float areaCirculo(float radio) {
+		final float PI = 3.141592f;
+		return  PI * radio * radio ;
+	}
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Introduce el radio: ");
+		float radio = scan.nextFloat();
+		
+		System.out.println("El área del círculo de radio " + radio + " es de " + areaCirculo(radio) + " unidades cuadradas.");
+	}
+}
---
+++ src/t1/Ej21.java
@@ -0,0 +1,39 @@
+package t1;
+
+import java.util.Scanner;
+
+public class Ej21 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Introduce segundos: ");
+		int segundos = scan.nextInt();
+
+		final int SPD = 24 * 60 * 60; 
+		int dias = segundos / SPD ;
+		int segundosRestantes = segundos % SPD;
+		
+		final int SPH = 60 * 60;
+		int horas = segundosRestantes / SPH ;
+		segundosRestantes = segundosRestantes % SPH ;
+		
+		final int SPM = 60;
+		int minutos = segundosRestantes / SPM ;
+		segundosRestantes = segundosRestantes % SPM ;
+
+		System.out.println(
+				segundos + 
+				" segundos son " +
+				dias +
+				" días , " +
+				horas +
+				" horas, " +
+				minutos +
+				" minutos y " +
+				segundosRestantes +
+				" segundos."
+				);
+	}
+}
