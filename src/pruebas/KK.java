d44bffb444928fd4f4394ab4e91ef6d40f912f41
HARRY <HARRY@potter>
2024-10-14 12:31:49 +0200

    Ej12

--- src/pruebas/KK.java
+++ src/pruebas/KK.java
@@ -3,9 +3,27 @@ package pruebas;
 public class KK {
 
 	public static void main(String[] args) {
+		int n1 = 2;
+		int n2 = 9;
+		int i;
 		
+		System.out.println("INI");
+
+		// OPCION 1
+		i = n1;
+		while (i <= n2) {
+			if ( i%2==1 ) {
+				System.out.println(i);
+			}
+			i = i + 1;
+		}
 		
-		
-		
+		// OPCION 2
+		i = (n1%2==0) ? n1+1 : n1;
+		while ( i<= n2) {
+			System.out.println(i);
+			i = i + 2;
+		}
+		System.out.println("FIN");
 	}
 }
