5a0df78506b3492c3ad93f3d64ee81354d7e478f
HARRY <HARRY@potter>
2024-10-30 11:02:38 +0100

    P01 todavía sin terminar

--- src/pruebas/Prueba.java
+++ src/pruebas/Prueba.java
@@ -1,11 +1,19 @@
 package pruebas;
 
+import java.util.Scanner;
+
 class Prueba {
 	public static void main(String[] args) {
-		for (int i = 0; i < 6; i++) {
-			System.out.println("=====");
-			System.out.println(i+1);
-			System.out.println("*****");
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Se pueden repetir los elementos (s/n): ");
+		String cadena = scan.nextLine();
+		
+		if (cadena.equals("s")) {
+			System.out.println("SI");
+		}
+		else {
+			System.out.println("NO");
 		}
 	}
 }
---
+++ src/t2/P01.java
@@ -0,0 +1,115 @@
+package t2;
+
+import java.util.Scanner;
+
+public class P01 {
+	public static String titulo(int n, int k, boolean repetir, boolean orden) {
+		String tituloFinal = "STANDARD";
+		if (orden) {
+			if (repetir) {
+				tituloFinal = "Variaciones CON repetición de " + n + " elementos tomados de " + k + " en " + k;
+			} else {
+				tituloFinal = "Variaciones de " + n + " elementos tomados de " + k + " en " + k;
+			}
+		} else {
+			if (repetir) {
+				tituloFinal = "Combinaciones CON repetición de " + n + " elementos tomados de " + k + " en " + k;
+			} else {
+				tituloFinal = "Combinaciones de " + n + " elementos tomados de " + k + " en " + k;
+			}
+		}
+		return tituloFinal + "\n=====================================================\n";
+	}
+
+	public static String tituloCorto(int n, int k, boolean repetir, boolean orden) {
+		String tituloFinal = "STANDARD";
+		if (orden) {
+			if (repetir) {
+				tituloFinal = "VR (" + n + "," + k + ")";
+			} else {
+				tituloFinal = "V (" + n + "," + k + ")";
+			}
+		} else {
+			if (repetir) {
+				tituloFinal = "CR (" + n + "," + k + ")";
+			} else {
+				tituloFinal = "C (" + n + "," + k + ")";
+			}
+		}
+		return tituloFinal ;
+	}
+
+	public static long solucion(int n, int k, boolean repetir, boolean orden) {
+		long solucion = 0;
+		if (orden) {
+			if (repetir) {
+				solucion = vr(n, k);
+			} else {
+				solucion = v(n, k);
+			}
+		} else {
+			if (repetir) {
+				solucion = cr(n, k);
+			} else {
+				solucion = c(n, k);
+			}
+		}
+		return solucion;
+	}
+
+	public static long fact(int n) {
+		long acumulado = 1;
+		for (long i=2;i<=n;i++) {
+			acumulado *= i;
+		}
+		return acumulado;
+	}
+
+	public static long cr(int n, int k) {
+		return 0L;
+	}
+
+	public static long c(int n, int k) {
+		long num  = 1 ;
+		long den  = (k<(n/2)) ? fact(k) : fact(n-k);
+		long cota = (k<(n/2)) ? (n-k+1) : (k+1);
+		for (long i=n; i >= cota; i-- ) {
+			num = num * i;
+		}
+		return num/den;
+	}
+
+	public static long vr(int n, int k) {
+		return 0L;
+	}
+
+	public static long v(int n, int k) {
+		return 0L;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Introduce n: ");
+		int n = scan.nextInt();
+
+		System.out.print("Introduce k: ");
+		int k = scan.nextInt();
+
+		System.out.print("Importa el orden (s/n): ");
+		String ordenString = scan.next();
+
+		System.out.print("Se pueden repetir los elementos (s/n): ");
+		String repetirString = scan.next();
+
+		System.out.println();
+
+		boolean repetir = (repetirString.equals("s")) ? true : false;
+		boolean orden = (ordenString.equals("s")) ? true : false;
+
+		System.out.println(titulo(n, k, repetir, orden));
+		System.out.println();
+		System.out.println(tituloCorto(n, k, repetir, orden) + " = " + solucion(n, k, repetir, orden));
+	}
+
+}
