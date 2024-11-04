d1a5858165dd03efa2586d99d2ff75b349bca98f
ALBERTO_IES <agaraycanas@educa.madrid.org>
2024-11-04 12:33:14 +0100

    t3/ej02

--- src/pruebas/Prueba.java
+++ src/pruebas/Prueba.java
@@ -2,15 +2,17 @@ package pruebas;
 
 class Prueba {
 
-	public static void f(int[] a) {
-		a[0] = 100;
+	public static int f(int n) {
+		n = n + 1;
+		System.out.println(n);
+		return n;
 	}
 
 	public static void main(String[] args) {
-		int[] miArray = { 10, 20, 30, 40, 50 };
-		System.out.println(miArray[0]);
-		f(miArray);
-		System.out.println(miArray[0]);
+		int n = 10;
+		System.out.println("==============");
+		System.out.println(f(n));
+		System.out.println("**************");
+		System.out.println(n);
 	}
-
 }
