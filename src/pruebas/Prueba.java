01da3fcae540f0d46f2276f4d5f831a982a16727
ALBERTO_IES <agaraycanas@educa.madrid.org>
2024-11-05 09:24:16 +0100

    t3/ej02

--- src/pruebas/Prueba.java
+++ src/pruebas/Prueba.java
@@ -1,18 +1,25 @@
 package pruebas;
 
+import java.util.Arrays;
+
 class Prueba {
 
-	public static int f(int n) {
-		n = n + 1;
-		System.out.println(n);
-		return n;
+	public static void f(int[] x) {
+		System.out.println("Inicio de f()");
+		System.out.println(Arrays.toString(x));
+		x[1] = x[1] + 1;
+		x[1] += 1;
+		x[1]++;
+		++x[1];
+		System.out.println(Arrays.toString(x));
+		System.out.println("Fin de f()");
 	}
 
 	public static void main(String[] args) {
-		int n = 10;
-		System.out.println("==============");
-		System.out.println(f(n));
-		System.out.println("**************");
-		System.out.println(n);
+		int[] n = { 10, 20} ;
+
+		System.out.println(Arrays.toString(n));
+		f(n);
+		System.out.println(Arrays.toString(n));
 	}
 }
--- src/t3/Ej02.java
+++ src/t3/Ej02.java
@@ -3,6 +3,38 @@ package t3;
 import java.util.Scanner;
 
 public class Ej02 {
+	public static int minimo(int[] numeros) {
+		int min = numeros[0];
+		for (int i=1; i<numeros.length && numeros[i]!=0 ;i++) {
+			if ( numeros[i] < min ) {
+				min = numeros[i];
+			}
+		}
+		return min;
+	}
+	public static int maximo(int[] numeros) {
+		int max = numeros[0];
+		for (int i=1; i<numeros.length && numeros[i]!=0 ;i++) {
+			if ( numeros[i] > max ) {
+				max = numeros[i];
+			}
+		}
+		return max;
+	}
+	
+	//===============================================================
+
+	public static int mediaAritmetica(int[] numeros) {
+		int sumaParcial = 0;
+		int contador;
+		for (contador=0; contador<numeros.length && numeros[contador]!=0 ; contador++) {
+			sumaParcial += numeros[contador];
+		}
+		return (contador!=0?sumaParcial/contador:0);
+	}
+	
+	//===============================================================
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		boolean seguir = true;
@@ -24,8 +56,10 @@ public class Ej02 {
 			System.out.print( ( i==0 ? "" : coma ) + n[i]);
 		}
 		
-		int media = 0;
 		System.out.println();
-		System.out.println("La media aritmética es: "+media);
+		System.out.println("El máximo es: " + maximo(n));
+		System.out.println("El mínimo es: " + minimo(n));
+		System.out.println("La media aritmética es: " + mediaAritmetica(n));
+		
 	}
 }
