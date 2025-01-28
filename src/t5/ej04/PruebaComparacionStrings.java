c82f93af594a12011972234356c2cbb41906fcd3
ALBERTO_IES <agaraycanas@educa.madrid.org>
2025-01-28 10:11:37 +0100

    t5/ej05

---
+++ src/t5/ej04/PruebaComparacionStrings.java
@@ -0,0 +1,12 @@
+package t5.ej04;
+
+public class PruebaComparacionStrings {
+
+	public static void main(String[] args) {
+		String corre = new String("corre");
+		String correcaminos = new String("correcaminos");
+
+		System.out.println(corre.compareTo(correcaminos)>0 ? "SI" : "NO");
+	}
+
+}
