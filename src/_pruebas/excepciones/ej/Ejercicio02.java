9b3049ed941525c5543cc2b123bc20c47d788655
ALBERTO_IES <agaraycanas@educa.madrid.org>
2025-02-19 08:08:39 +0100

    t8/ej04

--- src/_pruebas/excepciones/ej/Ejercicio02.java
+++ src/_pruebas/excepciones/ej/Ejercicio02.java
@@ -1,13 +1,17 @@
 package _pruebas.excepciones.ej;
 
 public class Ejercicio02 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
     	throw new A("PUM");
     }
  }
+
 class A extends RuntimeException {
 	public A(String m) {
 		super(m);
 	}
 }
-
+/*
+Throwable, Exception
+Error, RuntimeException
+*/
