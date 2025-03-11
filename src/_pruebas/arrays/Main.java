7f5774f1e54a6add1bd54b3361c864905175c55c
ALBERTO_IES <agaraycanas@educa.madrid.org>
2025-03-11 08:45:25 +0100

    t10/ej11 en curso ejercicio de arrays (max,min) también pendiente

---
+++ src/_pruebas/arrays/Main.java
@@ -0,0 +1,27 @@
+package _pruebas.arrays;
+
+import java.util.Arrays;
+
+public class Main {
+
+	private static int max(int[]a) {
+		int sol = 0;
+		return sol;
+	}
+
+	private static int min(int[]a) {
+		int sol = 0;
+		return sol;
+	}
+
+	
+	public static void main(String[] args) {
+		int[] a = { 4, 1, -10 , -1 , 7 , 100, 40 };
+		System.out.println("Dado el array ...");
+		System.out.println(Arrays.toString(a));
+		System.out.println("Máximo: " + max(a));
+		System.out.println("Mínimo: " + max(a));
+		
+	}
+
+}
