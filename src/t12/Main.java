24fb15bd95c85ab552bcca5d883e1d1ada44f6d9
ALBERTO_IES <agaraycanas@educa.madrid.org>
2025-04-04 09:10:07 +0200

    t12/ej02

---
+++ src/t12/Main.java
@@ -0,0 +1,17 @@
+package t12;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage s) throws Exception {
+		s.show();
+	}
+
+}
--- src/t12/ej02/Main.java
+++ src/t12/ej02/Main.java
@@ -11,27 +11,64 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	TextField nombre ;
-	Text panelSaludo ;
-	Button boton ;
- 
-	private void accion() {
-	}
+	private Text pantalla;
 	
 	@Override
 	public void start(Stage primaryStage) {
-		GridPane grid = new GridPane(2, 1);
-
+		GridPane grid = new GridPane();
+		/*
 		panelSaludo.setFont(Font.font("Verdana", 20));
 		boton.setFont(Font.font("Verdana", 20));
+		*/
+		int[] a = {
+				7,8,9,
+				4,5,6,
+				1,2,3,
+				-1,0,-2
+				};
 
-		grid.add(nombre, 0, 0);
-		grid.add(panelSaludo, 0, 1);
-		grid.add(boton, 0, 2);
+		this.pantalla = new Text("");
+		pantalla.setFont(Font.font("Verdana", 20));
+		grid.add(pantalla, 0, 0, 3,1);
 
+		for (int i=0;i<12;i++) {
+			String etiqueta = "";
+			if (a[i]>=0) {
+				etiqueta += a[i];
+			}
+			else {
+				etiqueta = a[i]==-1 ? "+" : "=";
+			}
+			Button boton = new Button(etiqueta);
+			boton.setFont(Font.font("Verdana", 20));
+			final String et = etiqueta;
+			boton.setOnAction( e -> accion(et) );
+			grid.add(boton, i%3, (i/3)+1);
+		}
+		
 		Scene s = new Scene(grid);
 		primaryStage.setScene(s);
 		primaryStage.show();
+	}
+
+	private void accion(String etiqueta) {
+		if (!etiqueta.equals("=")) {
+			this.pantalla.setText( pantalla.getText() + etiqueta ) ;
+		}
+		else {
+			calcularSuma();
+		}
+	}
+	
+	private void calcularSuma() {
+		String pString = this.pantalla.getText();
+		String[] apString = pString.split("\\+");
+		int sol = 0;
+		for (String numString : apString) {
+			int num = Integer.parseInt( numString );
+			sol += num;
+		}
+		this.pantalla.setText(sol+"");
 	}
 
 	public static void main(String[] args) {
