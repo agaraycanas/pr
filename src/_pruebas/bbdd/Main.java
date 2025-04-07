40ece9730f90edaa8affd2ef4fb8195d4b11f388
ALBERTO_IES <agaraycanas@educa.madrid.org>
2025-04-07 10:29:56 +0200

    ultima

--- src/_pruebas/bbdd/Main.java
+++ src/_pruebas/bbdd/Main.java
@@ -9,31 +9,40 @@ public class Main {
 
 	public static void main(String[] args)  {
 		System.out.println("ARRANCANDO");
-		//Class.forName("com.mysql.cj.jdbc.Driver");
 		
 		try {
+			System.out.println("Cargando DRIVER JDBC");
 			Class.forName("org.postgresql.Driver");
 		}
 		catch (Exception e) {
-			System.out.println("ERROR al cargar el DRIVER");
-			System.out.println(e.getMessage());
+			System.out.println("\tERROR al cargar el DRIVER");
+			System.out.println("\t"+e.getMessage());
 		}
 		
 		Connection con = null;
 		try {
-			con = DriverManager.getConnection("jdbc:postgresql://localhost/test", "root", "");
+			System.out.println("Conectando BD");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost/test", "postgres", "1234");
 		}
 		catch (Exception e) {
-			System.out.println("Error al conectar a la BD");
-			System.out.println(e.getMessage());
+			System.out.println("\tError al conectar a la BD");
+			System.out.println("\t"+e.getMessage());
 		}
 
-		/*
-		String sql = "delete from cliente";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ps.execute();
-		ps.close(); // RECOMENDABLE
-		*/
+		
+		String sql = "insert into cliente(dni,nombre) values (3,'Perico')";
+		try {
+			System.out.println("Ejecutando consulta SQL");
+			System.out.println(sql);
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.execute();
+			ps.close(); // RECOMENDABLE
+		}
+		catch (Exception e) {
+			System.out.println("Error al ejecutar SQL");
+			System.out.println(e.getMessage());
+		}
+		
 
 		System.out.println("FIN");
 	}
